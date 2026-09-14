69fbce471e1b2529922fdf223310be34817c261b
silver595 <akashpurjalkar66@gmail.com>
2026-09-14 19:18:40 +0530

    autoplay feature added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -89,6 +89,7 @@ const NavBar = () => {
                 className="hidden"
                 src="/audio/loop.mp3"
                 loop
+                autoPlay
               />
               {[1, 2, 3, 4].map((bar) => (
                 <div
